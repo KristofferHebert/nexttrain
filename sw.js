1d21d31b9d9aa085f388007fe5e97ab6cdefe87f
= <kristoffer@kristofferhebert.com>
2016-02-18 19:17:17 -0800

    feat: contined work ons sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
 // inspired by examples on
 // developer.mozilla.org/en-US/docs/Web/API/ServiceWorker.html
+// https://github.com/mxstbr/react-boilerplate/blob/master/serviceworker.js
+
+'use strict'
+
+const APPNAME = 'nexttrain-v1'
+
+const resourceCache = [
+    '/',
+    '/css/main.css',
+    '/js/bundle.js'
+]
 
 if(navigator.serviceWorker){
 
@@ -8,7 +19,7 @@ if(navigator.serviceWorker){
             scope: './'
         })
         .then(function(registration){
-            
+
         })
         .catch(function(error){
             console.log('ServiceWorker registration failed'. error)
