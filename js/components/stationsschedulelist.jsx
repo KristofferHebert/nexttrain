60fa7d252f74431c06c2d74fdfd418450ebf732f
= <kristoffer@kristofferhebert.com>
2016-03-21 19:56:40 -0700

    feat: adding schedule list

--- js/components/stationsschedulelist.jsx
+++ js/components/stationsschedulelist.jsx
@@ -30,20 +30,20 @@ const StationsScheduleList = React.createClass({
         const itemList = stationSchedule.station.item.map(function(item, i){
             return (
                 <li key={i} className='geosuggest-item'>
-                    <span className="small routes">{item.line} - #{item.trainHeadStation} - {item.trainIdx}</span>
-                    {item.origTime} to {item.destTime} {getDuration(date + " " + item.origTime, date + " " + item.destTime)}
+                    <span className="small routes">{item.line} - #{item.trainIdx} <span className="pull-right mr">{stationSchedule.name} to {item.trainHeadStation}</span></span>
+                    {item.origTime} to {item.destTime}  <span className="pull-right mr">Duration: {getDuration(date + " " + item.origTime, date + " " + item.destTime)}</span>
                 </li>
             )
         })
 
         return (
             <section>
-                <header>
-                    Station: {stationSchedule.fullname} <span className="pull-right">Date: {new Date().toDateString()}</span>
-                </header>
-                <ul className="geosuggest__suggests">
-                    {itemList}
-                </ul>
+                <p className="text-center">Scroll down to see more station times.</p>
+                <div className="overflow">
+                    <ul className="geosuggest__suggests">
+                        {itemList}
+                    </ul>
+                </div>
             </section>
             )
     }
--- js/util/duration.jsx
+++ js/util/duration.jsx
@@ -6,11 +6,11 @@ function getDuration(start, end) {
     const startDate = new Date(start).getTime()
     const endDate = new Date(end).getTime()
     const ms = endDate - startDate
-    
-    const hours = Math.floor(ms / 3600000)
-    const minutes = Math.floor((ms % 3600000) / 60000)
 
-    let duration = hours > 0 ? "hr " + minutes : ""
+    const hours = Math.abs(Math.floor(ms / 3600000))
+    const minutes = Math.abs(Math.floor((ms % 3600000) / 60000))
+
+    let duration = hours > 0 ? hours + "hr " + minutes : ""
     duration+= minutes + ' mins'
     return duration
 }
