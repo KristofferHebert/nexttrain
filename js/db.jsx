c0244ce133424155684d20c105d5409671ff1593
= <kristoffer@kristofferhebert.com>
2016-03-27 10:16:44 -0700

    feat: adding db

--- js/db.jsx
+++ js/db.jsx
@@ -1,20 +1,26 @@
 import Dexie from 'dexie'
 
-let db = new Dexie("BartDB")
+const DBNAME = "nexttrain-offline-stations"
+const VERSION = 1
 
-db.version(1).stores({ stations: "++id, name, details" })
+let db = new Dexie(DBNAME)
 
-db.open()
+db.version(VERSION).stores({ stations: "++id, name, station, fullname, sched_num" })
 
-db.stations.put({name: "12th", details: {
-    'test': 1,
-    'test': 2,
-    'test': 3
-}})
-    .then(() => {
-        return console.log(db.stations.get('12th'))
-   }).catch((error) => {
-      alert ("BartDB Save Failed: " + error)
-   })
+
+
+
+// db.open()
+
+// db.stations.put({name: "12th", details: {
+//     'test': 1,
+//     'test': 2,
+//     'test': 3
+// }})
+//     .then(() => {
+//         return console.log(db.stations.get('12th'))
+//    }).catch((error) => {
+//       alert ("BartDB Save Failed: " + error)
+//    })
 
 export default db
--- js/util/checkcacheorfetch.jsx
+++ js/util/checkcacheorfetch.jsx
@@ -1,19 +1,32 @@
 import makeRequest from 'makeRequest'
+import db from '../db.jsx'
 
-const checkCacheOrFetch = function(url){
+const checkCacheOrFetch = function(url, key){
     if(!url)
         throw new Error('missing url')
 
     // check DB for cached response
+    db.open()
 
-    // If not in DB, fetch then cache in DB
-    makeRequest()
-    then((response) => {
+    let stations = db.stations.where('name').equals(key)
 
-    })
-    catch(() => {
+    if(stations.length !== 0) {
+        stations.each((station) => {
+            
+        })
+    } else {
 
-    })
+        // If not in DB, fetch then cache in DB
+        makeRequest(url)
+        then((response) => {
+
+        })
+        catch(() => {
+
+        })
+
+    }
+
 }
 
 export default checkCacheOrFetch
